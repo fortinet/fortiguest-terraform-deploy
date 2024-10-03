326b4ac4881233f86dd091e328d5fc9cc65296b4
snaveenkumar <snaveenkumar@fortinet.com>
2024-10-03 12:26:03 +0530

    Development for Cloud platform deployments

---
+++ gcp/gcp/1.3.1/vars.tf
@@ -0,0 +1,45 @@
+# GCP region
+variable "region" {
+  type    = string
+  default = "us-central1" #Default Region
+}
+
+# GCP oauth access token
+variable "token" {
+  type    = string
+  default = "<Please add your OAuth Token>"
+}
+
+# GCP zone
+variable "zone" {
+  type    = string
+  default = "us-central1-a" #Default Zone
+}
+
+# GCP project name
+variable "project" {
+  type    = string
+}
+
+# GCP Fortinet official project
+variable "ftntproject" {
+  type    = string
+  default = "fortigcp-project-001"
+}
+
+variable "image" {
+  type    = string
+  default = "projects/fortigcp-project-001/global/images/fortiguest-131-build0167-lic"
+}
+
+# GCP instance machine type
+variable "machine" {
+  type    = string
+  default = "e2-standard-4"
+}
+
+# Public Subnet CIDR
+variable "public_subnet" {
+  type    = string
+  default = "172.16.0.0/24"
+}
